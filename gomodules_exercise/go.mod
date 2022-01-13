84e55f8cbe7b7792655a046b9a2dee8e417b4452
Gaurav Ranganathan <gaurav.ranganathan@gmail.com>
2022-01-13 11:58:52 -0800

    Create a sample app to understand and experiment with go modules

---
+++ gomodules_exercise/go.mod
@@ -0,0 +1,9 @@
+module github.com/gauravr007/golang-projects
+
+go 1.17
+
+require (
+	golang.org/x/text v0.3.7 // indirect
+	rsc.io/quote/v3 v3.1.0
+	rsc.io/sampler v1.99.99 // indirect
+)
---
+++ gomodules_exercise/main.go
@@ -0,0 +1,19 @@
+package gomodules_exercise
+
+import (
+	"fmt"
+
+	"rsc.io/quote/v3"
+)
+
+func main() {
+	fmt.Println("Hello")
+}
+
+func Hello() string {
+	return quote.HelloV3()
+}
+
+func Proverb() string {
+	return quote.Concurrency()
+}
